2b89166a12da3cb7e25d40650affa1a9c7cdc19f
Wilson Vallerian <wilsonvallerianwong@gmail.com>
2026-02-02 18:53:19 +0800

    Add drag-and-drop feature for tier items

--- src/selectors/selectors.js
+++ src/selectors/selectors.js
@@ -1,2 +1,24 @@
-export const selectUnplacedItems = (state) =>
-  state.unplacedItems.filter((item) => item.type === "unplaced");
+// export const selectItems = (state) => state.unplacedItems;
+
+export const selectItems = [
+  {
+    id: 20,
+    title: "Naruto",
+    image: "https://cdn.myanimelist.net/images/anime/1141/142503.jpg",
+  },
+  {
+    id: 37430,
+    title: "Slime",
+    image: "https://cdn.myanimelist.net/images/anime/1069/123309.jpg",
+  },
+  {
+    id: 3744,
+    title: "Isekai Cheat",
+    image: "https://cdn.myanimelist.net/images/anime/1282/102248.jpg",
+  },
+  {
+    id: 31964,
+    title: "BNHA",
+    image: "https://cdn.myanimelist.net/images/anime/10/78745.jpg",
+  },
+];
